cde24d1468731554e73a4b2d5a739ac1c0f44212
Marcos Solari <msolari@infosis.tech>
2023-09-25 14:01:23 -0300

    Create project structure

---
+++ spring-boot-security/src/test/java/com/markos96/springbootsecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markos96.springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
